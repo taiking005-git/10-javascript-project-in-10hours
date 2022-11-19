c4641202f61f9e52e0abb382273487168af85324
Kolawole Taiwo <taiking005.dev@gmail.com>
2022-11-19 20:14:43 +0100

    update

--- countdown timer/script.js
+++ countdown timer/script.js
@@ -8,20 +8,25 @@ const displaySeconds = document.getElementById("seconds");
 function getCountDown() {
     const currentYear = new Date();
     const newYear = new Date(newYearDate);
+    // =================== get date time difference between currentYear and newYear
     const milliSeconds = (newYear - currentYear) / 1000;
-
+    // =================== convert  milliSeconds to days, hours, minutes and seconds
     let days = Math.floor(milliSeconds / 3600 / 24);
     let hours = Math.floor(milliSeconds / 3600) % 24;
     let minutes = Math.floor(milliSeconds % 3600 / 60);
     let seconds = Math.floor(milliSeconds % 3600 % 60);
+    let obj = { "d": formatTime(days), "h": formatTime(hours), "m": formatTime(minutes), "s": formatTime(seconds) };
 
-    let obj = { "d": days, "h": hours, "m": minutes, "s": seconds };
-
+    // ================== Returning a date object
     return obj;
 }
 
-getCountDown()
+function formatTime(time) {
+    return time < 10 ? "0" + time : time;
+}
 
+
+// ============ set and interval to render the textContent every seconds
 setInterval(() => {
     displayDays.textContent = getCountDown().d;
     displayHours.textContent = getCountDown().h;
